b8f2ef4f0672cf9af5980ab70f66c10416c7bc87
eduard <eduardbargues@gmail.com>
2022-06-15 20:03:32 +0200

    second commit

--- api.users.js
+++ api.users.js
@@ -1,13 +1,25 @@
 const database = require("./db.users");
+const bcrypt = require("bcrypt");
+const constantNumber = 10;
 
-function signUp(request, response) {
+async function signUp(request, response) {
   const newUser = request.body;
   const users = database.getUsers();
-  const ids = users.map((u) => u.id);
-  newUser.id = Math.max([...ids]) + 1;
-  users.push(newUser);
-  database.saveUsers(users);
-  response.status(201).json({ id: newUser.id, userName: newUser.userName });
+  const sameUser = users.find((u) => u.userName === newUser.userName);
+  if (sameUser) {
+    response
+      .status(400)
+      .json({ message: "user with same userName already exists." });
+  } else {
+    const ids = users.map((u) => u.id);
+    newUser.id = Math.max([...ids]) + 1;
+    const salt = await bcrypt.genSalt(constantNumber);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
+    users.push(newUser);
+
+    database.saveUsers(users);
+    response.status(201).json({ id: newUser.id, userName: newUser.userName });
+  }
 }
 
 module.exports = {
